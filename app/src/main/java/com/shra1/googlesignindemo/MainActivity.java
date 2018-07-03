6fc18a701827a79f7655f547d4a30d8d857f7fd9
Shrawan WABLE <shra1w@gmail.com>
2018-07-03 15:08:20 +0530

    Initial commit

---
+++ app/src/main/java/com/shra1/googlesignindemo/MainActivity.java
@@ -0,0 +1,104 @@
+package com.shra1.googlesignindemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int RC_SIGN_IN = 241;
+    public static final String TAG = "ShraX";
+    GoogleSignInClient mGoogleSignInClient;
+    SignInButton sign_in_button;
+    TextView tvMADetails;
+    ImageView imageview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sign_in_button = findViewById(R.id.sign_in_button);
+        tvMADetails = findViewById(R.id.tvMADetails);
+        imageview = findViewById(R.id.imageview);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account != null) {
+            updateUI(account);
+        }
+
+        sign_in_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            //updateUI(null);
+        }
+    }
+
+    public void updateUI(GoogleSignInAccount account) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("Display Name : ").append(account.getDisplayName()).append("\n");
+        stringBuilder.append("Email : ").append(account.getEmail()).append("\n");
+        stringBuilder.append("Family Name : ").append(account.getFamilyName()).append("\n");
+        stringBuilder.append("Photo URL : ").append(account.getPhotoUrl().toString()).append("\n");
+
+
+        tvMADetails.setText(stringBuilder.toString());
+
+        Glide.with(MainActivity.this)
+                .load(account.getPhotoUrl().toString())
+                .into(imageview);
+
+    }
+}
